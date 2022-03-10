50a320178f6d66e709175cd2aab1570f98a96df7
EquilibrioApp <equilibrio.veri@gmail.com>
2022-03-10 14:22:08 -0600

    Corrigio modulo de indices

--- src/indices/indices.module.ts
+++ src/indices/indices.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { IndicesService } from './src/indices/indices.service';
-import { IndicesController } from './src/indices/indices.controller';
+import { IndicesService } from './indices.service';
+import { IndicesController } from './indices.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { IndiceEntity } from './src/indices/indice.entity';
+import { IndiceEntity } from './indice.entity';
 import { PesoEntity } from 'src/pesos/peso.entity';
 import { CircunferenciaEntity } from 'src/circunferencias/circunferencia.entity';
 
